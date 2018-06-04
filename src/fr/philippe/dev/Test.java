8ae48fa0afcc7ab8af02f4b96bb08c2d3ba083c3
Philippe BIDOT <ph.bidot@gmail.com>
2018-06-04 18:48:15 +0200

    config.properties en écriture

--- src/fr/philippe/dev/Test.java
+++ src/fr/philippe/dev/Test.java
@@ -1,8 +1,41 @@
 package fr.philippe.dev;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
+
 public class Test {
 
 	public static void main(String[] args) {
+		Properties properties = new Properties();
+		OutputStream out = null;
 		
+		try {
+			out = new FileOutputStream("config.properties");
+
+			properties.setProperty("driver", "mysql");
+			properties.setProperty("database", "localhost");
+			properties.setProperty("port", "3306");
+			properties.setProperty("user", "root");
+			properties.setProperty("password", "12345");
+		
+			properties.store(out, null);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			if (out != null)
+			{
+				try {
+					out.close();
+					out = null;
+				}
+				catch (IOException i) {
+					i.printStackTrace();
+				}
+			}
+		}
 	}
 }
